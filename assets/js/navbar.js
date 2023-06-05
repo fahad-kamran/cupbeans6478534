06af4564f6f7d20046e30313a41e1d0000463fc0
fahad-kamran <fahadcs512@gmail.com>
2023-06-05 22:51:59 +0500

    remove async from navbar.js 2

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,26 +1,28 @@
-let drawer = document.getElementById('drawer');
-let toogleDrawer = document.getElementById('toogle-drawer');
+document.addEventListener('DOMContentLoaded', function () {
+    const drawer = document.getElementById('drawer');
+    const toogleDrawer = document.getElementById('toogle-drawer');
 
-const desktopScreen = () => {
-    drawer.classList.remove('offcanvas', 'offcanvas-start', 'bgSecondary');
-    toogleDrawer.classList.add('d-none');
-}
-const mobileScreen = () => {
-    drawer.classList.add('offcanvas', 'offcanvas-start', 'bgSecondary');
-    toogleDrawer.classList.remove('d-none');
-}
-if (window.innerWidth < 768) {
-    mobileScreen();
-}
-else {
-    desktopScreen();
-}
-const handleWindowResize = () => {
+    const desktopScreen = () => {
+        drawer.classList.remove('offcanvas', 'offcanvas-start', 'bgSecondary');
+        toogleDrawer.classList.add('d-none');
+    }
+    const mobileScreen = () => {
+        drawer.classList.add('offcanvas', 'offcanvas-start', 'bgSecondary');
+        toogleDrawer.classList.remove('d-none');
+    }
     if (window.innerWidth < 768) {
         mobileScreen();
     }
     else {
         desktopScreen();
     }
-}
-window.addEventListener('resize', handleWindowResize);
+    const handleWindowResize = () => {
+        if (window.innerWidth < 768) {
+            mobileScreen();
+        }
+        else {
+            desktopScreen();
+        }
+    }
+    window.addEventListener('resize', handleWindowResize);
+});
